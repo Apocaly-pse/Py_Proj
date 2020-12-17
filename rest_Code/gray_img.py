a0cf7027d2ece4f8c5f5b4b6e49cba5f5f1791e5
Apocaly-pse <apocaly_pse@qq.com>
2020-12-17 09:14:21 +0800

    surface提交

---
+++ rest_Code/gray_img.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = Image.open('cugb_title.png')
+img = np.array(img)
+if img.ndim == 3:
+    img = img[:,:,0]
+
+plt.imshow(img, cmap = plt.cm.gray_r)
+plt.show()

04b1fa8cae5b72bc48e91677ce3e4983f6fd3604
Apocaly-pse <apocaly_pse@outlook.com>
2021-05-06 16:15:15 +0800

    0506-1

--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-def convertToTitle(n: int) -> str:
-    col = {}
-    # 首先定义字典存储对应关系
-    for i in range(26):
-        col[i] = chr(65 + i)
-
-    if n <= 26:
-        return col[n]
-    elif n <= 26**2 + 26:
-        return col[(n-1)//26-1] + col[(n-1)%26]
-    elif n <= 26**3 + 26:
-        return col[n // 26 % 26] + col[n // 26 // 26] + col[n % 26]
-    elif n <= 26**4 + 26:
-        return col[n // 26 // 26 % 26] + col[n // 26 // 26 // 26] + col[n // 26 % 26] + col[n % 26]
-    elif n <= 26**5 + 26:
-        return col[n // 26 // 26 // 26 % 26] + col[n // 26 // 26 // 26 // 26] + col[n // 26 // 26 % 26] + col[n // 26 % 26] + col[n % 26]
-    elif n <= 26**6 + 26:
-        return col[n // 26 // 26 // 26 // 26 % 26] + col[n // 26 // 26 // 26 // 26 // 26%26] + col[n // 26 // 26 // 26 % 26] + col[n // 26 // 26 % 26] + col[n // 26 % 26] + col[n % 26]
-    elif n <= 26**7 + 26:
-        return col[n // 26 // 26 // 26 // 26 // 26 // 26%26] + col[n // 26 // 26 // 26 // 26 // 26 % 26] + col[n // 26 // 26 // 26 // 26 % 26] + col[n // 26 // 26 // 26 % 26] + col[n // 26 // 26 % 26] + col[n // 26 % 26] + col[n % 26]
+def findMaxAverage(nums, k: int) -> float:
+    ret = sum(nums[:k])
+    for i in range(len(nums) - k):
+        ret = max(ret - nums[i] + nums[i + k], ret)
+    return ret / k
 
 
-n = 52
-print(convertToTitle(n))
+print(findMaxAverage([0, 4, 0, 3, 1], 1))
